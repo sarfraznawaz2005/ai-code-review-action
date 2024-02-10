157a062fed3564cf13948b40f7821ac7222e7323
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-02-11 01:56:20 +0500

    updated

---
+++ README.md
@@ -0,0 +1,38 @@
+# Gemini AI Code Review Tool
+
+This action takes as input `GIT_TOKEN` and `GEMINI_API_KEY` and watches pull/push requests and sends the git diff to AI and comments (in case of PR only) on the PR as well as sending details in email if configured.
+
+Usage:
+
+1 - You need to set up your project's permissions so that the Github Actions can write comments on Pull Requests. You can read more about this here: [automatic-token-authentication](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token)
+
+2 - Set `GIT_TOKEN` and `GEMINI_API_KEY`  [as action secrets in your repository](https://docs.github.com/en/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-a-repository)
+
+3 - Finally, create a file named `review-action.yml`  inside `.github/workflows` with the following contents:
+
+```
+name: ChatGPT PR Code Review
+
+on:
+  push:
+  pull_request:
+    types: [opened, reopened, synchronize]
+
+jobs:
+  review:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Gemini AI Code Review Action
+        uses: sarfraznawaz2005/ai-code-review@1.0.8
+        with:
+          github-token: ${{ secrets.GIT_TOKEN }}
+          gemini-api-key: ${{ secrets.GEMINI_API_KEY }}
+          email-host: 'xxxxxxxxxxxxxxxxx'
+          email-port: 'xxxxxxxxxxxxxxxxx'
+          email-user: 'xxxxxxxxxxxxxxxxx'
+          email-pass: 'xxxxxxxxxxxxxxxxx
+          email-from: 'xxxxxxxxxxxxxxxxx'
+          email-to: 'xxxxxxxxxxxxxxxxx'
+          email-bcc: 'xxxxxxxxxxxxxxxxx'
+          email-secure: 'true' or 'false'
+```
---
+++ src/index.js
@@ -0,0 +1,201 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+const nodemailer = require('nodemailer');
+const showdown = require('showdown');
+
+const converter = new showdown.Converter({
+    omitExtraWLInCodeBlocks: true,
+    simplifiedAutoLink: true,
+    strikethrough: true
+});
+
+async function getPrDiff(prNumber, octokit, repo) {
+    const { data: diff } = await octokit.rest.pulls.get({
+        owner: repo.owner,
+        repo: repo.repo,
+        pull_number: prNumber,
+        mediaType: { format: 'diff' },
+    });
+
+    return diff;
+}
+
+async function getPushDiff(commits, octokit, repo) {
+    let diffs = '';
+    for (const commit of commits) {
+        const { data: commitData } = await octokit.rest.repos.getCommit({
+            owner: repo.owner,
+            repo: repo.repo,
+            ref: commit.id,
+        });
+
+        diffs += commitData.files.map(file => `Commit: ${commit.id}\nFile: ${file.filename}\n${file.patch}\n`).join('\n');
+    }
+
+    return diffs;
+}
+
+async function getExplanation(diff, geminiApiKey, model) {
+    const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${geminiApiKey}`;
+
+    const prompt = `
+    Act as a code reviewer with deep knowledge of software development. Always start your suggestions with file name(s). 
+    Provide suggestions on how to make code more readable, maintainable and secure including line numbers with each suggestion. 
+    
+    Rules you must follow:
+    - Only consider code files not images or videos for example.
+    - Don't suggest about indentation and readability or spacing issues.
+    
+    Here is code: 
+    ${diff}
+    `;
+
+    try {
+
+        const postData = {
+            contents: [
+                {
+                    role: 'user',
+                    parts: [
+                        {
+                            text: prompt
+                        }
+                    ]
+                }
+            ],
+            generationConfig: {
+                maxOutputTokens: 4096,
+                //temperature: 1.0,
+                //topP: 0.8,
+                //topK: 10
+            }
+        };
+
+        const response = await axios.post(apiUrl, postData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        //console.log(response);
+
+        return response.data.candidates[0].content.parts[0].text || 'Error or no response!';
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function commentOnPr(prNumber, explanation, octokit, repo) {
+    await octokit.rest.issues.createComment({
+        owner: repo.owner,
+        repo: repo.repo,
+        issue_number: prNumber,
+        body: explanation,
+    });
+}
+
+async function sendEmail(subject, body, emailConfig) {
+
+    if (!emailConfig.host || !emailConfig.to) {
+        console.log("No email credentials confirued!");
+    }
+    else {
+
+        const transporter = nodemailer.createTransport({
+            host: emailConfig.host,
+            port: emailConfig.port,
+            secure: emailConfig.secure, // true for 465, false for other ports
+            auth: {
+                user: emailConfig.user,
+                pass: emailConfig.pass,
+            },
+            tls: {
+                ciphers: 'SSLv3',
+                rejectUnauthorized: false
+            }
+        });
+
+        const mailOptions = {
+            from: emailConfig.from,
+            to: emailConfig.to,
+            bcc: emailConfig.bcc,
+            subject: subject,
+            html: body,
+        };
+
+        await transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email:', error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+}
+
+async function run() {
+    try {
+        const token = core.getInput('github-token', { required: true });
+        const geminiApiKey = core.getInput('gemini-api-key', { required: true });
+        const model = core.getInput('model');
+        const octokit = github.getOctokit(token);
+        const repo = github.context.repo;
+
+        const emailConfig = {
+            host: core.getInput('email-host'),
+            port: core.getInput('email-port'),
+            secure: core.getInput('email-secure') === 'true',
+            user: core.getInput('email-user'),
+            pass: core.getInput('email-pass'),
+            from: core.getInput('email-from'),
+            to: core.getInput('email-to'),
+            bcc: core.getInput('email-bcc'),
+        };
+
+        let subject = '';
+        let body = '';
+
+        if (github.context.eventName === 'pull_request') {
+            const prNumber = github.context.payload.pull_request.number;
+            const diff = await getPrDiff(prNumber, octokit, repo);
+            const explanation = await getExplanation(diff, geminiApiKey, model);
+
+            await commentOnPr(prNumber, explanation, octokit, repo);
+
+            subject = `Code Review: Pull Request #${prNumber} in ${repo.repo.toUpperCase()}`;
+            body = explanation;
+
+        } else if (github.context.eventName === 'push') {
+            const commits = github.context.payload.commits;
+
+            if (!commits || commits.length === 0) {
+                console.log('No commits found in push event.');
+                return;
+            }
+
+            const diff = await getPushDiff(commits, octokit, repo);
+            const explanation = await getExplanation(diff, geminiApiKey, model);
+
+            subject = `Code Review: Push Event in ${repo.repo.toUpperCase()}`;
+            body = explanation;
+            console.log(`explanation`);
+            console.log(explanation);
+        }
+
+        if (body) {
+            body = converter.makeHtml(body);
+
+            await sendEmail(subject, body, emailConfig);
+        }
+
+        core.setOutput("success", true);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+
+run();
